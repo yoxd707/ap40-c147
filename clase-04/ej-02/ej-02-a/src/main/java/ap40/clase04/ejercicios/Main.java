7d29ea7e9c3cbf713632373b70b22a42df994dc4
Castaño Ulises <ulises27.000@gmail.com>
2023-03-06 22:28:20 -0300

    Resolución del ejercicio 2-A de la clase 4.

---
+++ clase-04/ej-02/ej-02-a/src/main/java/ap40/clase04/ejercicios/Main.java
@@ -0,0 +1,28 @@
+package ap40.clase04.ejercicios;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main( String[] args ) throws IOException {
+        // 2) Haga una main donde por parámetro envíe la ruta de un archivo. Ese archivo debe
+        // contener números.
+        // a) El programa debe escribir por consola la suma de esos números.
+
+        //* Adjunto ya un archivo con los números, dir: src/main/resources/numeros.txt
+
+        String dir = (args.length == 0)
+            ? "src/main/resources/numeros.txt"
+            : args[0];
+
+        boolean dirEsAbsoluto = (args.length < 2)
+            ? true
+            : Boolean.parseBoolean(args[1]);
+
+        int[] numeros = AdquisitorDeNumeros.adquirirEnterosDesdeArchivo(dir, dirEsAbsoluto);
+        int sumatoria = Sumatoria.sumatoriaDeEnteros(numeros);
+
+        System.out.println(sumatoria);
+    }
+
+}
